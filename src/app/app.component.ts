7ec76c5cc67a4b50055625888b646a053e027a21
Kenneth Phang <bunnyppl@gmail.com>
2018-09-05 15:35:39 +0800

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { 
+    trigger,
+    state,
+    style,
+    transition,
+    animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('divState',[
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('hightlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> hightlighted', animate(400))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('hightlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => hightlighted', animate(300)),
+      transition('hightlighted => normal', animate(800)),
+      transition('shrunken <=> *',[
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        }))
+      , animate(800)
+    ])
+  ])
+  ]
 })
 export class AppComponent {
+  state="normal";
+  wildState = "normal";
+
   title = 'ng-animation';
+
+  onAnimate(){
+    this.state=='normal'? this.state = 'hightlighted' : this.state = 'normal';
+    this.wildState == 'normal'? this.wildState = 'hightlighted': this.wildState = 'normal'
+  }
+
+  onShrink(){
+    this.wildState = "shrunken";
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ButtonsModule } from 'ngx-bootstrap';
+
+import { CarouselModule } from 'ngx-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
